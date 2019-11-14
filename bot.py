f98e4b0db232d6677568a4778fce33691dff5ee4
bularond <bularond@gmail.com>
2019-11-14 18:08:30 +0300

    Refactor keyboard

--- bot.py
+++ bot.py
@@ -5,9 +5,9 @@ import logging
 from settings import token
 from database import database
 
-from telegram import ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
-                          ConversationHandler)
+                          ConversationHandler, CallbackQueryHandler)
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -15,18 +15,19 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-MENU, CHOOSING = range(2)
+MENU, CHOOSING, LOOKING = range(3)
 
 db = database()
 
-keyboard = [["Catalog"],
-            ["Reviews"],
-            ["Guarantee"],
-            ["Support"]
-        ]
-markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
-
 def start(update, context):
+    menu_keyboard = [
+        ["Каталог"],
+        ["Отзывы"],
+        ["Гарантии"],
+        ["Поддержка"]
+    ]
+    markup = ReplyKeyboardMarkup(menu_keyboard, one_time_keyboard=True)
+
     update.message.reply_text("Entire message",
             reply_markup=markup)
 
@@ -35,26 +36,64 @@ def start(update, context):
 
 def catalog(update, context):
     text = update.message.text
+    if(context.user_data.get('offset') == None):
+        context.user_data['offset'] = 0
     reply_text = 'List:\n'
     point = 1
-    for item in db.get_catalog():
+    ls = db.get_catalog(offset=context.user_data['offset'])
+    for item in ls:
         reply_text += f"{point}. {item['name']} - {item['cost']} p.\n"
         point += 1
-    keyboard = [
-            [InlineKeyboardButton(str(i), callback_data=str(i)) for i in range(j, min(point, j+5))] 
-            for j in range(1, point, 5)
-    ] + [[InlineKeyboardButton('Back', callback_data='back')]]
-    reply_markup = InlineKeyboardMarkup(keyboard)
+    keyboard = list()
+    for i in range(1, point, 5):
+        keyboard.append([])
+        for j in range(i, min(point, i + 5)):
+            keyboard[-1].append(str(j))
+    keyboard.append([
+        'Back'
+    ])
+    reply_markup = ReplyKeyboardMarkup(keyboard)
     update.message.reply_text(reply_text, reply_markup=reply_markup)
-    
+
     return CHOOSING
+
+
+def catalog_button(update, context):
+    text = update.message.text
+    if(text == 'Back'):
+        return start(update, context)
+    else:
+        id = context.user_data['offset'] + int(text) - 1
+        context.user_data['last_id'] = id
+        item = db.get_item_by_id(id)
+        reply_text = f"{item['name']}\n{item['description']}\n{item['cost']} p."
+        keyboard = [
+            [KeyboardButton('Buy'), KeyboardButton('Back')]
+        ]
+        reply_markup = ReplyKeyboardMarkup(keyboard)
+        update.message.reply_text(reply_text, reply_markup=reply_markup)
+
+        return LOOKING
+
+
+def looking_button(update, context):
+    text = update.message.text
+    if(text == 'Back'):
+        return catalog(update, context)
+    elif(text == 'Buy'):
+        #TODO
+        update.message.reply_text(f"Buy id={context.user_data['last_id']}")
+        return catalog(update, context)
+
 
 def other(update, context):
     pass
 
+
 def done(update, context):
     update.message.reply_text("Something went wrong")
     return ConversationHandler.END
+
 
 def error(update, context):
     """Log Errors caused by Updates."""
@@ -72,9 +111,17 @@ def main():
                                catalog),
                 MessageHandler(Filters.regex('^(Reviews|Guarantee|Support)$'),
                                other)
+            ],
+            CHOOSING: [
+                MessageHandler(Filters.regex('^(0|1|2|3|4|5|6|7|8|9|10|Back)$'),
+                               catalog_button)
+            ],
+            LOOKING: [
+                MessageHandler(Filters.regex('^(Buy|Back)$'),
+                               looking_button)
             ]
         },
-        fallbacks=[MessageHandler(Filters.regex('^Done$'), done)]
+        fallbacks=[]
     )
 
     dp.add_handler(conv_handler)
--- database.py
+++ database.py
@@ -4,31 +4,37 @@ class database:
 
     ls = [
         {
+            'id': 0,
             'name': 'A',
             'cost': 100,
             'description': 'aaaaa'
         },
         {
+            'id': 1,
             'name': 'B',
             'cost': 110,
             'description': 'bbbbb'
         },
         {
+            'id': 2,
             'name': 'C',
             'cost': 120,
             'description': 'ccccc'
         },
         {
+            'id': 3,
             'name': 'D',
             'cost': 130,
             'description': 'ddddd'
         },
         {
+            'id': 4,
             'name': 'E',
             'cost': 140,
             'description': 'eeeee'
         },
         {
+            'id': 5,
             'name': 'F',
             'cost': 150,
             'description': 'fffff'
@@ -40,3 +46,6 @@ class database:
 
     def get_catalog(self, count=10, offset=0):
         return self.ls[offset:offset+count] 
+
+    def get_item_by_id(self, id):
+        return self.ls[id]
